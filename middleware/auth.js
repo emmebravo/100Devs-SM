0546d4978dc7c3ddf14865cf70d32a43ccdf7cde
mbcodes <mbcodes@protonmail.com>
2021-05-03 19:04:30 -0700

    editing sm

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,16 @@
 module.exports = {
-    ensureAuth: function (req, res, next) {
-      if (req.isAuthenticated()) {
-        return next()
-      } else {
-        res.redirect('/')
-      }
-    },
-    ensureGuest: function (req, res, next) {
-      if (!req.isAuthenticated()) {
-        return next()
-      } else {
-        res.redirect('/profile')
-      }
-    },
-  }
-  
+  ensureAuth: function (request, response, next) {
+    if (request.isAuthenticated()) {
+      return next();
+    } else {
+      response.redirect('/');
+    }
+  },
+  ensureGuest: function (request, response, next) {
+    if (!request.isAuthenticated()) {
+      return next();
+    } else {
+      response.redirect('/profile');
+    }
+  },
+};
